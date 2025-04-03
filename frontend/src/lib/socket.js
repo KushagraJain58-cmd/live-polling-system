864486b401bc99e753b6c3115dea5e42ac917d63
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2025-04-03 23:55:34 +0530

    Update socket.js

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client"
 
 // Initialize socket connection
-export const socket = io( "http://localhost:3001", {
+export const socket = io( "https://live-polling-system-1.onrender.com", {
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: 5,
